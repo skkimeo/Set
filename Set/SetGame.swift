28f9fb3d60dfab6841965df1782b40eb614228bb
sun <skkimeo@gmail.com>
2021-10-07 15:31:36 +0900

    - Model : created "Card Struct" and "Init" - viewModel : made the squareCardContent

--- Set/SetGame.swift
+++ Set/SetGame.swift
@@ -7,17 +7,42 @@
 
 import Foundation
 
-struct SetGame<CardContent>{
-    private(set) var cards: [Card]
+struct SetGame<CardShape, CardColor, CardPattern, NumberOfShapes>{
+    private(set) var cardDeck: [Card]
+    private(set) var playingCards: [Card]
     
-    func choose(_ card: Card) {
+    mutating func choose(_ card: Card) {
         
     }
     
+    mutating func deal_three_cards() {
+        
+    }
+    
+    init(createCardContent: (Int) -> Card.CardContent) {
+        cardDeck = []
+        playingCards = []
+        for index in 0..<81 {
+            let content = createCardContent(index)
+            if index < 12 {
+                playingCards.append(Card(content: content))
+            } else {
+                cardDeck.append(Card(content: content))
+            }
+        }
+    }
+    
     struct Card {
-        let Content: CardContent  // shape, color, pattern
+        let content: CardContent
         let isFaceUp = false
         let isMatched = false
+        
+        struct CardContent {
+            let shape: CardShape
+            let color: CardColor
+            let pattern: CardPattern
+            let numberOfShapes: Int
+        }
     }
 }
 
--- Set/SquareSetGame.swift
+++ Set/SquareSetGame.swift
@@ -7,9 +7,67 @@
 
 import SwiftUI
 
-struct SquareSetGame {
+class SquareSetGame: ObservableObject {
+    typealias Card = SetGame<Shape, Color, Pattern, NumberOfShapes>.Card
     
-    struct CardContent {
+    static let cardContents: [Card.CardContent] = {
+        var contents = [Card.CardContent]()
         
+        for shape in Shape.allCases {
+            for color in Color.allCases {
+                for pattern in Pattern.allCases {
+                    for numberOfShapes in NumberOfShapes.allCases {
+                        contents.append(Card.CardContent( shape: shape, color: color, pattern: pattern, numberOfShapes: numberOfShapes.rawValue))
+                    }
+                }
+            }
+        }
+        return contents
+    }()
+    
+    static private func createSetGame() -> SetGame<Shape, Color, Pattern, NumberOfShapes> {
+        SetGame { cardContents[$0] }
     }
+    
+    
+    @Published private var model = createSetGame()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    enum Shape: CaseIterable {
+        case roundedRectangle
+        case diamond
+        case square
+    }
+    
+    enum Color: CaseIterable {
+        case red
+        case green
+        case purple
+    }
+    
+    enum Pattern: CaseIterable {
+        case filled
+        case stroked
+        case shaeded
+    }
+    
+    enum NumberOfShapes: Int, CaseIterable {
+        case one = 1
+        case two
+        case three
+    }
+    
+//    struct CardContent {
+//        let shape: Shape
+//        let color: Color
+//        let pattern: Pattern
+//        let numberOfShapes: NumberOfShapes
+//    }
 }
