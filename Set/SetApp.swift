c8afc7c70d97d716594e8892dcc7ad465b35142e
sun <skkimeo@gmail.com>
2021-10-07 10:18:07 +0900

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by sun on 2021/10/07.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
